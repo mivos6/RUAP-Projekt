b22b3caf3e6b10118f1b957cf9c0f50c45339538
Milan <Milan>
2016-04-12 10:44:08 +0200

    Dodao aplikaciju.

---
+++ ruap/ruap/ruap.cpp
@@ -0,0 +1,12 @@
+// ruap.cpp : Defines the entry point for the console application.
+//
+
+#include <stdio.h>
+
+
+int main()
+{
+	printf("Pozdrav iz aplikacije Milan Ivosevic.");
+    return 0;
+}
+
